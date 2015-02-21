3408964c4802b20637d51fdd6b66fba353026729
CUAgrad74 <github@patlombard.com>
2015-02-21 16:41:21 -0500

    Modified shell program cachematrix.R to compute and return the inverse of a passed matrix using cache when possible

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,65 @@
-## Put comments here that give an overall description of what your
-## functions do
+## cachematrix.R
 
-## Write a short comment describing this function
+## R_Programming class
+## ProgrammingAssignment2 for week 3
+## 20-Feb-2015
+## CUAgrad74
+
+## There are two main functions in the program: makeCacheMatrix and cacheSolve.
+## The purpose of the functions is to return the inverse of a passed matrix argument.
+
+## The inverse of the matrix is computed if it has not been computed previously
+## and it is then cached into another matrix object.
+## If the inverse has been computed previously, it is retreived from the cache
+## instead of being computed again.
+
+
+## The makeCacheMatrix function defines two matrices that are combined into
+## the list cacheMatrix: origmtrx contains the originally passed matrix
+## and invrtmtrx contains its inverse. invrtmtrx is initially set to NULL.
+## A logical incache reflects whether the inverse of the argument matrix is
+## currently cached in invrtmrtx.  The list cacheMatrix is returned.
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  origmtrx <<- matrix()
+  invrtmtrx <<- matrix()
+
+  incache <<- FALSE
+  
+  origmtrx <<- x
+  invrtmtrx <<- NULL
+  
+  cacheMatrix <<- list(original=origmtrx, inverse=invrtmtrx)
 }
 
 
-## Write a short comment describing this function
+## The cacheSolve function returns a matrix that is the inverse of the matrix
+## argument passed in the function call. If the inverse of that matrix has already
+## been computed and is in cache, the cached matrix is returned.  If the inverse
+## of the matrix is not already in cache, it is computed and assigned to cacheMatrix$inverse.
+## The logical incache is set to TRUE. cacheMatrix$inverse is returned.
+
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+
+  if (!identical(cacheMatrix$original, x)) {  ## These are different matrices, can't use cache
+    
+      message("new matrix passed, computing inverse")  
+      cacheMatrix$original <<- x
+      incache <<- FALSE
+      }
+    
+  else if (incache) {
+        message("using cached inverse matrix")
+        return(cacheMatrix$inverse)     
+    }
+
+  cacheMatrix$inverse <<- solve(x)
+  incache <<- TRUE
+  
+  return(cacheMatrix$inverse)
 }
+
+## End cachematrix.R
